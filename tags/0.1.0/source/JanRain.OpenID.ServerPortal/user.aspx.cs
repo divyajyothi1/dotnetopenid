1a2a790d210e2828e61d93c10d9370c5239f49aa
andrewarnott <andrewarnott@01efa1a6-402a-0410-b0ae-47b76eba00f0>
2008-01-30 15:52:42 +0000

    Tagging 0.1 version

---
+++ tags/0.1.0/source/JanRain.OpenID.ServerPortal/user.aspx.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+/// <summary>
+/// This page is a required as part of the service discovery phase of the openid protocol (step 1).
+/// 
+/// How does a url like http://www.myserver.com/user/bob map to  http://www.myserver.com/user.aspx?username=bob ?
+/// Check out gobal.asax and the URLRewriter class. Essentially there's a little framework that allows for URLRewrting using the HttpContext.Current.RewritePath method.
+/// 
+/// A url such as http://www.myserver.com/user/bob which is entered on  the consumer side will cause this page to be invoked.
+/// This page must be parsed by the openid compatible consumer and the url of the openid server is extracted from href in:  rel="openid.server" href="?".
+/// It is the responsibility of the consumer to redirect the user to this url.
+/// 
+/// The XRDS (or Yadis) content is also rendered to provide the consumer with an alternative discovery mechanism. The Yadis protocol allows the consumer 
+/// to provide the user with a more flexible range of authentication mechanisms (which ever has been defined in xrds.aspx). See http://en.wikipedia.org/wiki/Yadis.
+/// </summary>
+public partial class user : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+    private int ServerPort
+    {
+        get
+        {
+            // Return the same port that the request was made on,
+            // unless it is overrided by a Web.config setting and is
+            // non-negative.
+            string sPort = ConfigurationManager.AppSettings["OverrideServerPort"];
+            int iPort;
+            if (string.IsNullOrEmpty(sPort) || (iPort = Convert.ToInt32(sPort)) < 0)
+                return Request.Url.Port;
+            else
+                return iPort;
+        }
+    }
+
+    public string ServerUrl
+    {
+        get
+        {
+            UriBuilder builder = new UriBuilder(Request.Url);
+            builder.Path = Response.ApplyAppPathModifier("~/server.aspx");
+            builder.Query = null;
+            builder.Port = ServerPort;
+            builder.Fragment = null;
+            return builder.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Gets the name of the user.
+    /// </summary>
+    /// <value>The name of the user.</value>
+    public string UserName
+    {
+        get
+        {
+            return Request.QueryString["username"];
+        }
+    }
+
+    public string XrdsUrl
+    {
+        get
+        {
+            UriBuilder builder = new UriBuilder(Request.Url);
+            builder.Path = Response.ApplyAppPathModifier("~/xrds.aspx");
+            builder.Query = null;
+            builder.Port = ServerPort;
+            builder.Fragment = null;
+            return builder.ToString();
+        }
+    }    
+}
